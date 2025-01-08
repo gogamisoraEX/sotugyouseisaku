24933809867cf3e491c38f4c064fae0726cb873a
a-min <shinzm.etak@gmail.com>
2025-01-09 04:39:35 +0900

    Update カウントscript.js

--- カウントscript.js
+++ カウントscript.js
@@ -1,5 +1,7 @@
-const mozi1 = document.querySelector("name1");
-
+const mozi1 = document.querySelector("#name1");
+const mozi2 = document.querySelector("#name2");
+const mozi3 = document.querySelector("#name3");
+const mozi4 = document.querySelector("#name4");
 const number1 = document.querySelector(".i");
 const number2 = document.querySelector(".ii");
 const number3 = document.querySelector(".iii");
@@ -18,20 +20,41 @@ let count3 = 0;
 let count4 = 0;
 
 window.addEventListener("load", () => {
-    const storednum1 = localStorage.getItem('mozi1');
+  const storedCount1 = localStorage.getItem('count1');
+  const storedCount2 = localStorage.getItem('count2');
+  const storedCount3 = localStorage.getItem('count3');
+  const storedCount4 = localStorage.getItem('count4');
+  const storedName1 = localStorage.getItem('name1');
+  const storedName2 = localStorage.getItem('name2');
+  const storedName3 = localStorage.getItem('name3');
+  const storedName4 = localStorage.getItem('name4');
+  count1 = storedCount1 ? JSON.parse(storedCount1) : 0;
+  count2 = storedCount2 ? JSON.parse(storedCount2) : 0;
+  count3 = storedCount3 ? JSON.parse(storedCount3) : 0;
+  count4 = storedCount4 ? JSON.parse(storedCount4) : 0;
 
-    const storedCount1 = localStorage.getItem('count1');
-    const storedCount2 = localStorage.getItem('count2');
-    const storedCount3 = localStorage.getItem('count3');
-    const storedCount4 = localStorage.getItem('count4');
-    count1 = storedCount1 ? JSON.parse(storedCount1) : 0;
-    count2 = storedCount2 ? JSON.parse(storedCount2) : 0;
-    count3 = storedCount3 ? JSON.parse(storedCount3) : 0;
-    count4 = storedCount4 ? JSON.parse(storedCount4) : 0;
-    number1.innerHTML = count1;
-    number2.innerHTML = count2;
-    number3.innerHTML = count3;
-    number4.innerHTML = count4;
+  number1.innerHTML = count1;
+  number2.innerHTML = count2;
+  number3.innerHTML = count3;
+  number4.innerHTML = count4;
+
+    mozi1.value = storedName1;
+    mozi2.value = storedName2;
+    mozi3.value = storedName3;
+    mozi4.value = storedName4;
+});
+
+mozi1.addEventListener('input', () => {
+  localStorage.setItem('name1', mozi1.value);
+});
+mozi2.addEventListener('input', () => {
+  localStorage.setItem('name2', mozi2.value);
+});
+mozi3.addEventListener('input', () => {
+  localStorage.setItem('name3', mozi3.value);
+});
+mozi4.addEventListener('input', () => {
+  localStorage.setItem('name4', mozi4.value);
 });
 
 plusButton1.addEventListener("click", () => {
@@ -39,10 +62,10 @@ plusButton1.addEventListener("click", () => {
     count1++;
     number1.innerHTML = count1;
     if(count1 == 4) {
-      alert(mozi1 + 'の授業の欠席が多いです');
+      alert(mozi1.value + "の欠席数が多いです");
     }
     localStorage.setItem('count1', JSON.stringify(count1));
-    localStorage.setItem('mozi1', mozi1);
+    localStorage.setItem('mozi1', mozi1.value);
   }
 });
 
@@ -50,7 +73,11 @@ plusButton2.addEventListener("click", () => {
   if(count2 < 5) {
     count2++;
     number2.innerHTML = count2;
+    if(count2 == 4) {
+      alert(mozi2.value + "の欠席数が多いです");
+    }
     localStorage.setItem('count2', JSON.stringify(count2));
+    localStorage.setItem('mozi2', mozi2.value);
   }
 });
 
@@ -58,7 +85,11 @@ plusButton3.addEventListener("click", () => {
   if(count3 < 5) {
     count3++;
     number3.innerHTML = count3;
+    if(count3 == 4) {
+      alert(mozi3.value + "の欠席数が多いです");
+    }
     localStorage.setItem('count3', JSON.stringify(count3));
+    localStorage.setItem('mozi3', mozi3.value);
   }
 });
 
@@ -66,7 +97,11 @@ plusButton4.addEventListener("click", () => {
   if(count4 < 5) {
     count4++;
     number4.innerHTML = count4;
+    if(count4 == 4) {
+      alert(mozi4.value + "の欠席数が多いです");
+    }
     localStorage.setItem('count4', JSON.stringify(count4));
+    localStorage.setItem('mozi4', mozi4.value);
   }
 });
 
@@ -75,6 +110,7 @@ minusButton1.addEventListener("click", () => {
     count1--;
     number1.innerHTML = count1;
     localStorage.setItem('count1', JSON.stringify(count1));
+    localStorage.setItem('mozi1', mozi1.value);
   }
 });
 
@@ -83,6 +119,7 @@ minusButton2.addEventListener("click", () => {
     count2--;
     number2.innerHTML = count2;
     localStorage.setItem('count2', JSON.stringify(count2));
+    localStorage.setItem('mozi2', mozi2.value);
   }
 });
 
@@ -91,6 +128,7 @@ minusButton3.addEventListener("click", () => {
     count3--;
     number3.innerHTML = count3;
     localStorage.setItem('count3', JSON.stringify(count3));
+    localStorage.setItem('mozi3', mozi3.value);
   }
 });
 
@@ -99,5 +137,6 @@ minusButton4.addEventListener("click", () => {
     count4--;
     number4.innerHTML = count4;
     localStorage.setItem('count4', JSON.stringify(count4));
+    localStorage.setItem('mozi4', mozi4.value);
   }
 });
